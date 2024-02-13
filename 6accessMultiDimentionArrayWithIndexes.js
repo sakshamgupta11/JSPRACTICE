f3a23e198f7299ec09709401b475f1c7d142805e
SakshamGupta <sakshamgupta2308@gmail.com>
2024-02-13 13:45:19 +0530

    access MultiDimention Array With Indexes.

---
+++ 6accessMultiDimentionArrayWithIndexes.js
@@ -0,0 +1,10 @@
+const arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [10,11,12, [13,14,25]]
+]
+console.log(arr[3][3][0]) // output 13
+console.log(arr[0][1]) // output 2
+console.log(arr[2]) // output 7,8,9
+console.log(arr[3]) // 10,11,12,13,14,25
