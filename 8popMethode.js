55c0b922b28b939cd93d99ffa888e7674cc75821
SakshamGupta <sakshamgupta2308@gmail.com>
2024-02-14 10:07:22 +0530

    pop and splice  methode

---
+++ 8popMethode.js
@@ -0,0 +1,12 @@
+const arr = [12,13,14,15,16];
+console.log(arr,"\n")
+// using push methode to use remove last  an element from array
+
+arr.push()
+console.log(arr);
+
+// using splice methode to use any else element of array 
+arr.splice(4,1)
+console.log(arr) // output 12,13,14,15
+arr.splice(2,1)
+console.log(arr) // output 12, 13, 15
