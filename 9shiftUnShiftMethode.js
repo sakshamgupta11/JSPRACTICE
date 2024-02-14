fe80b5f214bd2155b920a632ad69bb1e646f9bf7
SakshamGupta <sakshamgupta2308@gmail.com>
2024-02-14 10:54:08 +0530

    shift unshift methode

---
+++ 9shiftUnShiftMethode.js
@@ -0,0 +1,8 @@
+const arr = ["saksham","gupta","coder"];
+
+arr.shift(); // pop methode is use in remove last element of an array but shift methode is use to remove first element of an array  
+console.log(arr) 
+
+arr.unshift("saksham"); // push methode is use in add last element of an array but unshift methode is use to add first element of an array  
+
+console.log(arr)
