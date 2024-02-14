862e2b4f6f898d103cb4f51f8c09242cdc719c25
SakshamGupta <sakshamgupta2308@gmail.com>
2024-02-14 10:34:22 +0530

    add pop also

--- 7pushmethod.js
+++ 7pushmethod.js
@@ -2,6 +2,9 @@
 const arr = [23,11,25];
 console.log(arr,"\n");
 //  using push methode to add string or number in arr.
-arr.push([33,44,78])
-console.log(arr) // output --> 23,11,25,12,33,44
+arr.push(33,44,78)
+
+console.log(arr) 
+arr.pop()
+console.log(arr)
 
