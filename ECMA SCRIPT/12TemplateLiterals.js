5add6792a9493267da84df3fa355c5d4d0db25d8
SakshamGupta <sakshamgupta2308@gmail.com>
2024-02-19 04:05:09 +0530

    comment added

--- ECMA SCRIPT/12TemplateLiterals.js
+++ ECMA SCRIPT/12TemplateLiterals.js
@@ -7,6 +7,8 @@ const myFun =(arr)=>{
  const myarray = [];
     for(let i=0; i < arr.length; i++){
         myarray[i] =(`this is an error of ${arr[i]}`)
+        // or
+        // myarray.pudh(`this is an error of ${arr[i]}`)
        
     }
     return myarray;
